4fb31990fb0374f219043cea8f560535ed57f267
Kevin Wu <kevin.wu@live.com.my>
2018-08-15 16:20:14 +0800

    create index and bulk insert

--- Readme.md
+++ Readme.md
@@ -6,3 +6,14 @@ npm i -g pkg
 [Build on Windows 10, Node 8.x]
 pkg elasti-cli.js --target node8-win-x64 --output elasti-cli.exe
 
+Download Elasticsearch
+https://www.elastic.co/downloads/elasticsearch
+
+Extract zip file,
+Run bin/elasticsearch (or bin\elasticsearch.bat on Windows)
+Default service running at http://localhost:9200
+
+[Test elastisearch is running]
+./elasti-cli get
+./elasti-cli get '_cat'
+
--- elasti-cli.js
+++ elasti-cli.js
@@ -16,6 +16,16 @@ const fullUrl = (path = '') => {
   return url + path.replace(/^\/*/, '');  // avoid extra slash in the URL
 };
 
+const handleResponse = (err, res, body) => {
+  if (commander.json) {
+    console.log(JSON.stringify(err || body));
+  }
+  else {
+    if (err) { throw err; }
+    console.log(body);
+  }
+}
+
 commander
   .version(pkg.version)
   .description(pkg.description)
@@ -47,6 +57,57 @@ commander
         if (err) { throw err; }
         console.log(body);
       }
+    });
+  });
+
+commander
+  .command('create-index')
+  .description('create an index')
+  .action(() => {
+    if (!commander.index) {
+      const msg = 'No index specified! Use --index <name>';
+      if (!commander.json) { throw Error(msg); }
+      console.log(JSON.stringify({ error: msg }));
+      return;
+    }
+    request.put(fullUrl(), handleResponse);
+  });
+
+commander
+  .command('list-indices')
+  .alias('li')
+  .description('get a list of indices')
+  .action(() => {
+    const path = commander.json ? '_all' : '_cat/indices?v';
+    request({ url: fullUrl(path), json: commander.json }, handleResponse);
+  });
+
+commander
+  .command('bulk-insert <file>')
+  .description('read and perform bulk inserts from the specified file')
+  .action((file) => {
+    fs.stat(file, (err, stats) => {
+      if (err) {
+        if (commander.json) {
+          console.log(JSON.stringify(err));
+          return;
+        }
+        throw err;
+      }
+
+      const options = {
+        url: fullUrl('_bulk'),
+        json: true,
+        headers: {
+          'content-length' : stats.size,
+          'content-type' : 'application/x-ndjson'
+        }
+      };
+
+      const req = request.post(options);
+      const stream = fs.createReadStream(file);
+      stream.pipe(req);
+      req.pipe(process.stdout);
     });
   });
 
